376dba109b46df63731c931b26a2d2261874bb9d
Yannick Beaudoin <yannickb@gmail.com>
2025-07-20 10:05:54 -0400

    Toggle RNG

--- random.js
+++ random.js
@@ -2,6 +2,10 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const randomNumberDiv = document.getElementById("randomNumberDiv");
+    const toggleButton = document.getElementById("toggleButton");
+    let autoRefresh = true;
+    let interval;
+    let refreshInternval = 2500;
   
     function generateRandomNumber() {
       return Math.floor(Math.random() * 101); // Generates a random number between 0 and 100
@@ -20,7 +24,27 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
 
+    function startAutoRefresh() {
+      interval = setInterval(updateRandomNumber, refreshInternval);
+    }
+
+    function stopAutoRefresh() {
+        clearInterval(interval);
+    }
+
+    toggleButton.addEventListener("click", function() {
+        autoRefresh = !autoRefresh;
+        if (autoRefresh) {
+            startAutoRefresh();
+            toggleButton.innerText = "Disable Auto Refresh";
+        } else {
+            stopAutoRefresh();
+            toggleButton.innerText = "Enable Auto Refresh";
+        }
+    });
+
     updateRandomNumber();
+    startAutoRefresh();
   
     randomNumberDiv.addEventListener("click", updateRandomNumber);
   });
